8015dbe8163ab7f45d2191ba235fd5578d71b0ad
Dominik R. Kocher <dominikraphael.kocher@students.bfh.ch>
2018-11-16 13:50:51 +0100

    MIDATA persistence (profile -> currentWeight)

--- src/pages/welcome-capture/welcome-capture.ts
+++ src/pages/welcome-capture/welcome-capture.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { WelcomeContactPage } from '../welcome-contact/welcome-contact';
 import { Storage } from '@ionic/storage';
+import { BodyWeight } from 'Midata';
+import { MidataService } from '../../services/MidataService';
 
 /**
  * Generated class for the WelcomeCapturePage page.
@@ -20,8 +22,9 @@ export class WelcomeCapturePage {
   inputtext:string;
   key:string="username";
 
+  currentWeight;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage, private midataService: MidataService) {
   }
 
   ionViewDidLoad() {
@@ -32,11 +35,16 @@ export class WelcomeCapturePage {
     this.navCtrl.push(WelcomeContactPage, {});
   }
   
+  //saves the data locally and also on MIDATA
   saveData() {
     this.storage.set(this.key, this.inputtext);
     this.storage.get(this.key).then((val) => {
       console.log('Your username is', val);
     });
+
+    //MIDATA persistance
+    this.midataService.save(new BodyWeight(+this.currentWeight, new Date().toISOString()));
+
   }
 
 }
