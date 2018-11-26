a40a9c03b008913ade98e6c92a6d17e3b0deb776
Burcu Sevinc <burcu.sevinc@students.bfh.ch>
2018-11-26 23:57:32 +0100

    Contacts Lokal Storage and Validation of Fields

--- src/pages/welcome-contact2/welcome-contact2.ts
+++ src/pages/welcome-contact2/welcome-contact2.ts
@@ -4,6 +4,8 @@ import { WelcomeContact3Page } from '../welcome-contact3/welcome-contact3';
 import { WelcomeContactPage } from '../welcome-contact/welcome-contact';
 
 import { Contacts, Contact, ContactField, ContactName } from '@ionic-native/contacts';
+import { Storage } from '@ionic/storage';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 /**
  * Generated class for the WelcomeContact2Page page.
@@ -19,7 +21,66 @@ import { Contacts, Contact, ContactField, ContactName } from '@ionic-native/cont
 })
 export class WelcomeContact2Page {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private contacts: Contacts) {
+  //Key Value Storage
+  bezugsperson_inputtext:string;
+  key:string="bezugsperson_inputtext";
+
+  bezugsperson_telefonnummer:any;
+  key1:any="bezugsperson_telefonnummer";
+
+  bezugsperson_smstext:string;
+  key2:string="bezugsperson_smstext";
+
+  bezugsperson_email:string;
+  key3:string="bezugperson_email";
+
+  bezugsperson_emailtext:string;
+  key4:string="bezugsperson_emailtext";
+
+  //For Validation of Fields
+  formgroup:FormGroup;
+  inputtext:AbstractControl;
+  telefonnummer:AbstractControl;
+  smstext:AbstractControl;
+  email:AbstractControl;
+  emailtext:AbstractControl;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    private storage: Storage,
+    private contacts: Contacts,
+    private formBuilder: FormBuilder ) {
+
+      //Validation of Inputfields
+      this.formgroup = formBuilder.group({
+        inputtext:['', Validators.required],
+        telefonnummer:['', Validators.required],
+        smstext:['', Validators.required],
+        email:['', Validators.required],
+        emailtext:['', Validators.required]
+      });
+
+      this.inputtext = this.formgroup.contains['inputtext'];
+      this.telefonnummer = this.formgroup.contains['telefonnummer'];
+      this.smstext = this.formgroup.contains['smstext'];
+      this.email = this.formgroup.contains['email'];
+      this.emailtext = this.formgroup.contains['emailtext'];
+
+  }
+
+  //Lokal Storage  
+  saveData() {
+    this.storage.set(this.key, this.bezugsperson_inputtext);
+    this.storage.set(this.key1, this.bezugsperson_telefonnummer);
+    this.storage
+    .set(this.key2, this.bezugsperson_smstext);
+    this.storage.set(this.key3, this.bezugsperson_email);
+    this.storage.set(this.key4, this.bezugsperson_emailtext);
+    /**For testing 
+    this.storage.get(this.key), this.storage.get(this.key1).then((values) => {
+      console.log('Your username is', values);
+    }); */
   }
 
   ionViewDidLoad() {
