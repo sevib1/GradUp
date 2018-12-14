7d58b4c7a1130e8c802b84e7da10b9a7e0d01bc9
sandraburri <sandra.burri@students.bfh.ch>
2018-12-14 12:08:57 +0100

    login logout fixed

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, MenuController, NavParams, App } from 'ionic-angular';
+import { IonicPage, NavController, MenuController, NavParams, LoadingController, App } from 'ionic-angular';
 import { ProfileImpressumPage } from '../profile-impressum/profile-impressum';
 import { ProfilePrivacyPage } from '../profile-privacy/profile-privacy';
 import { ProfileTermsPage } from '../profile-terms/profile-terms';
@@ -7,9 +7,9 @@ import { ProfileCustomizePage } from '../profile-customize/profile-customize';
 import { ProfileCustomizecontactsPage } from '../profile-customizecontacts/profile-customizecontacts';
 import { ProfileBiovotionPage } from '../profile-biovotion/profile-biovotion';
 import { ProfileAccessrightsPage } from '../profile-accessrights/profile-accessrights';
-import { WelcomePage } from '../welcome/welcome';
-
+import { JournalPage } from '../journal/journal';
 import { MidataService } from '../../services/MidataService';
+import { Page } from 'ionic-angular/umd/navigation/nav-util';
 
 @IonicPage()
 @Component({
@@ -18,7 +18,17 @@ import { MidataService } from '../../services/MidataService';
 })
 export class ProfilePage {
 
-  constructor(public app: App, private midataService: MidataService, public navCtrl: NavController, private menuCtrl: MenuController, public navParams: NavParams) {
+  nextPage: Page;
+
+  constructor(
+    public app: App,
+    private midataService: MidataService,
+    public navCtrl: NavController,
+    private menuCtrl: MenuController,
+    private loadingCtrl: LoadingController,
+    public navParams: NavParams
+  ) {
+    this.nextPage = JournalPage;
   }
 
   ionViewDidLoad() {
@@ -58,11 +68,33 @@ export class ProfilePage {
     this.midataService.logout()
       .then(() => {
         this.menuCtrl.close();
-        this.app.getRootNav().setRoot(WelcomePage);
+        this.app.getRootNav().setRoot(JournalPage);
 
       })
       .catch(() => {
-        this.app.getRootNav().setRoot(WelcomePage);
+        this.app.getRootNav().setRoot(JournalPage);
+      })
+  }
+
+  // Login
+  public goNext() {
+    let loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+
+    loading.present().catch();
+
+    this.midataService.authenticate()
+      .then((success: boolean) => {
+        return this.navCtrl.setRoot(this.nextPage)
+      })
+      .then(() => {
+        loading.dismiss().catch();
+      })
+      .catch((error) => {
+        console.log(error);
+        console.log(this.midataService.getNetworkState());
+        loading.dismiss().catch();
       })
   }
 
