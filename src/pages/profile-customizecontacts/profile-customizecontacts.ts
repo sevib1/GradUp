bd3564077aed1ad29caaade412d669b7616770fa
sandraburri <sandra.burri@students.bfh.ch>
2019-01-02 17:50:22 +0100

    Customize / create contacts on profile page completed. Conditions work

--- src/pages/profile-customizecontacts/profile-customizecontacts.ts
+++ src/pages/profile-customizecontacts/profile-customizecontacts.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProfilePage } from '../profile/profile';
 
 import { Storage } from '@ionic/storage';
+import { Validators, AbstractControl, FormBuilder, FormGroup } from '@angular/forms';
+import { validatePhoneIfNotEmpty, validateEmailIfNotEmpty } from '../../services/validators';
 
 @IonicPage()
 @Component({
@@ -18,21 +20,39 @@ export class ProfileCustomizecontactsPage {
 
   bezugsperson_inputtext = '';
   bezugsperson_telefonnummer = '';
-  bezugsperson_sms = '';
+  bezugsperson_smstext = '';
   bezugsperson_email = '';
   bezugsperson_emailtext = '';
 
   fachperson: string;
   key: string = "fachperson";
 
+  isSubmitted: boolean = false;
+  formGroup: FormGroup;
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private storage: Storage, ) {
+    private storage: Storage,
+    formBuilder: FormBuilder
+  ) {
+
+    this.formGroup = formBuilder.group({
+      bezugsperson_inputtext: [''],
+      bezugsperson_telefonnummer: ['', validatePhoneIfNotEmpty],
+      bezugsperson_email: ['', validateEmailIfNotEmpty],
+      bezugsperson_emailtext: [''],
+      bezugsperson_smstext: [''],
+
+      fachperson_inputtext: [''],
+      fachperson_telefonnummer: ['', validatePhoneIfNotEmpty],
+      fachperson_email: ['', validateEmailIfNotEmpty],
+      fachperson_emailtext: ['']
+    });
 
     this.getData();
   }
-  
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfileCustomizecontactsPage');
   }
@@ -40,49 +60,58 @@ export class ProfileCustomizecontactsPage {
   getData() {
     let that = this;
 
+    // TODO: move to separate service...
     this.storage.get('bezugsperson_inputtext').then((value) => {
-      that.bezugsperson_inputtext = value || 'Kein Eintrag vorhanden';
+      that.bezugsperson_inputtext = value;
     });
 
     this.storage.get('bezugsperson_telefonnummer').then((value) => {
-      that.bezugsperson_telefonnummer = value || 'Kein Eintrag vorhanden';
+      that.bezugsperson_telefonnummer = value;
     });
 
     this.storage.get('bezugsperson_smstext').then((value) => {
-      that.bezugsperson_sms = value || 'Kein Eintrag vorhanden';
+      that.bezugsperson_smstext = value;
     });
 
-    this.storage.get('bezugperson_email').then((value) => {
-      that.bezugsperson_email = value || 'Kein Eintrag vorhanden';
+    this.storage.get('bezugsperson_email').then((value) => {
+      that.bezugsperson_email = value;
     });
 
     this.storage.get('bezugsperson_emailtext').then((value) => {
-      that.bezugsperson_emailtext = value || 'Kein Eintrag vorhanden';
+      that.bezugsperson_emailtext = value;
     });
 
     this.storage.get('fachperson_inputtext').then((value) => {
-      that.fachperson_inputtext = value || 'Kein Eintrag vorhanden';
+      that.fachperson_inputtext = value;
     });
 
     this.storage.get('fachperson_telefonnummer').then((value) => {
-      that.fachperson_telefonnummer = value || 'Kein Eintrag vorhanden';
+      that.fachperson_telefonnummer = value;
     });
 
     this.storage.get('fachperson_email').then((value) => {
-      that.fachperson_email = value || 'Kein Eintrag vorhanden';
+      that.fachperson_email = value;
     });
 
     this.storage.get('fachperson_emailtext').then((value) => {
-      that.fachperson_emailtext = value || 'Kein Eintrag vorhanden';
+      that.fachperson_emailtext = value;
     });
   }
 
   saveData() {
+
+    this.isSubmitted = true;
+ 
+    if (!this.formGroup.valid) {
+      return
+    }
+
+    // TODO: move to separate service...
     let saveAll = [
       this.storage.set('bezugsperson_inputtext', this.cleanValue(this.bezugsperson_inputtext)),
       this.storage.set('bezugsperson_telefonnummer', this.cleanValue(this.bezugsperson_telefonnummer)),
-      this.storage.set('bezugsperson_smstext', this.cleanValue(this.bezugsperson_sms)),
-      this.storage.set('bezugperson_email', this.cleanValue(this.bezugsperson_email)),
+      this.storage.set('bezugsperson_smstext', this.cleanValue(this.bezugsperson_smstext)),
+      this.storage.set('bezugsperson_email', this.cleanValue(this.bezugsperson_email)),
       this.storage.set('bezugsperson_emailtext', this.cleanValue(this.bezugsperson_emailtext)),
       this.storage.set('fachperson_inputtext', this.cleanValue(this.fachperson_inputtext)),
       this.storage.set('fachperson_telefonnummer', this.cleanValue(this.fachperson_telefonnummer)),
