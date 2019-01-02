04e9ae09c85f91ebb7969863909fa140892c2035
sandraburri <sandra.burri@students.bfh.ch>
2019-01-02 14:30:21 +0100

    Enter sales activity related person completed. Conditions work

--- src/pages/welcome-contact2/welcome-contact2.ts
+++ src/pages/welcome-contact2/welcome-contact2.ts
@@ -3,9 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { WelcomeContact3Page } from '../welcome-contact3/welcome-contact3';
 import { WelcomeContactPage } from '../welcome-contact/welcome-contact';
 
-import { Contacts, Contact, ContactField, ContactName } from '@ionic-native/contacts';
 import { Storage } from '@ionic/storage';
-import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms'; 
 
 /**
  * Generated class for the WelcomeContact2Page page.
@@ -22,63 +21,93 @@ import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/fo
 export class WelcomeContact2Page {
 
   //Key Value Storage
-  bezugsperson_inputtext:string;
-  key:string="bezugsperson_inputtext";
+  bezugsperson_inputtext: string;
+  key: string = "bezugsperson_inputtext";
 
-  bezugsperson_telefonnummer:any;
-  key1:any="bezugsperson_telefonnummer";
+  bezugsperson_telefonnummer: any;
+  key1: any = "bezugsperson_telefonnummer";
 
-  bezugsperson_smstext:string;
-  key2:string="bezugsperson_smstext";
+  bezugsperson_smstext: string;
+  key2: string = "bezugsperson_smstext";
 
-  bezugsperson_email:string;
-  key3:string="bezugsperson_email";
+  bezugsperson_email: string;
+  key3: string = "bezugsperson_email";
 
-  bezugsperson_emailtext:string;
-  key4:string="bezugsperson_emailtext";
+  bezugsperson_emailtext: string;
+  key4: string = "bezugsperson_emailtext";
+
+  isSubmitted: boolean = false;
 
   //For Validation of Fields
-  formgroup:FormGroup;
-  inputtext:AbstractControl;
-  telefonnummer:AbstractControl;
-  smstext:AbstractControl;
-  email:AbstractControl;
-  emailtext:AbstractControl;
+  formGroup: FormGroup;
+  inputtext: AbstractControl;
+  telefonnummer: AbstractControl;
+  smstext: AbstractControl;
+  email: AbstractControl;
+  emailtext: AbstractControl;
 
   constructor(
-    public navCtrl: NavController, 
-    public navParams: NavParams, 
+    public navCtrl: NavController,
+    public navParams: NavParams,
     private storage: Storage,
-    private contacts: Contacts,
-    private formBuilder: FormBuilder ) {
-
-      //Validation of Inputfields
-      this.formgroup = formBuilder.group({
-        inputtext:['', Validators.required],
-        telefonnummer:['', Validators.required],
-        smstext:['', Validators.required],
-        email:['', Validators.required],
-        emailtext:['', Validators.required]
-      });
-
-      this.inputtext = this.formgroup.contains['inputtext'];
-      this.telefonnummer = this.formgroup.contains['telefonnummer'];
-      this.smstext = this.formgroup.contains['smstext'];
-      this.email = this.formgroup.contains['email'];
-      this.emailtext = this.formgroup.contains['emailtext'];
+    formBuilder: FormBuilder
+  ) {
+  
+    this.formGroup = formBuilder.group({
+      inputtext: [
+        ''
+      ],
+      telefonnummer: [
+        '',
+        (control: AbstractControl) => {
+          var result = null;
+          if (control.value) {
+            // https://regex101.com/r/BBMPv2/2
+            result = Validators.pattern(/^(\+41|0)\s?(\d{2})\s?(\d{3})\s?(\d{2})\s?(\d{2})$/)(control);
+          }          
+          console.debug('validate telefonnummer', control.value, result);
+          return result;
+        }
+      ],
+      smstext: [
+        ''
+      ],
+      email: [
+        '',
+        (control: AbstractControl) => {
+          var result = null;
+          if (control.value) {
+            result = Validators.email(control);
+          }
+          console.debug('validate email', control.value, result);
+          return result;
+        }
+      ],
+      emailtext: [
+        ''
+      ]
+    });
   }
 
   //Lokal Storage  
   saveData() {
-    this.storage.set(this.key, this.bezugsperson_inputtext);
-    this.storage.set(this.key1, this.bezugsperson_telefonnummer);
-    this.storage.set(this.key2, this.bezugsperson_smstext);
-    this.storage.set(this.key3, this.bezugsperson_email);
-    this.storage.set(this.key4, this.bezugsperson_emailtext);
-    /**For testing 
-    this.storage.get(this.key), this.storage.get(this.key1).then((values) => {
-      console.log('Your username is', values);
-    }); */
+
+    this.isSubmitted = true;
+ 
+    this.formGroup.valid
+    if (!this.formGroup.valid) {
+      return
+    }
+
+    Promise.all([
+      this.storage.set(this.key, this.bezugsperson_inputtext),
+      this.storage.set(this.key1, this.bezugsperson_telefonnummer),
+      this.storage.set(this.key2, this.bezugsperson_smstext),
+      this.storage.set(this.key3, this.bezugsperson_email),
+      this.storage.set(this.key4, this.bezugsperson_emailtext)
+    ]).then(() => {
+      this.gotoWelcomeContact3Page();
+    })  
   }
 
   ionViewDidLoad() {
